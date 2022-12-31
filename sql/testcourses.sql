054f56ae4fd268a982eb0e537f06f4adc59a4a7b
Andreas Meier <meier89@gmx.ch>
2022-12-31 07:50:57 +0100

    Add courses for test data

---
+++ sql/testcourses.sql
@@ -0,0 +1,16 @@
+INSERT INTO course (course_id, course_name, created_at)
+VALUES (0, 'breads', '2022-12-21 20:02:38');
+INSERT INTO course (course_id, course_name, created_at)
+VALUES (1, 'condiments_preserves', '2022-12-21 20:02:38');
+INSERT INTO course (course_id, course_name, created_at)
+VALUES (2, 'desserts', '2022-12-21 20:02:38');
+INSERT INTO course (course_id, course_name, created_at)
+VALUES (3, 'dips', '2022-12-21 20:02:38');
+INSERT INTO course (course_id, course_name, created_at)
+VALUES (4, 'drinks', '2022-12-21 20:02:38');
+INSERT INTO course (course_id, course_name, created_at)
+VALUES (5, 'mains', '2022-12-21 20:02:38');
+INSERT INTO course (course_id, course_name, created_at)
+VALUES (6, 'sides', '2022-12-21 20:02:38');
+INSERT INTO course (course_id, course_name, created_at)
+VALUES (7, 'snacks', '2022-12-21 20:02:38');
