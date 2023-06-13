e75f764d39a249eb7e4a822c5a1ac49af00cc99c
Andreas Meier <meier89@gmx.ch>
2023-06-13 08:44:13 +0200

    Fix extraction of domains without 'www.'

--- src/strops.rs
+++ src/strops.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 
 pub fn extract_domain(a: String) -> String {
-    let start = Regex::new(r"^https?://www.").unwrap();
+    let start = Regex::new(r"^https?://(www.)?").unwrap();
     let end = Regex::new("/.*$").unwrap();
     let b = start.replace(a.as_str(), "");
     let temp = b.to_string();
@@ -17,6 +17,15 @@ mod tests {
     #[test]
     fn guardian() {
         let asdf = "https://www.theguardian.com/international".to_string();
-        assert_eq!("theguardian.com", extract_domain(asdf))
+        assert_eq!("theguardian.com", extract_domain(asdf));
+        let fdsa = "https://www.theguardian.com/food/2022/aug/24/nigel-slater-midweek-dinner-recipe-baked-peaches-cinnamon-cream-sauce";
+        assert_eq!("theguardian.com", extract_domain(fdsa.to_string()));
+    }
+
+    #[test]
+    fn nyt(){
+        let url = "https://cooking.nytimes.com/recipes/1017018-shoofly-pie".to_string();
+        assert_eq!("cooking.nytimes.com", extract_domain(url))
+
     }
 }
